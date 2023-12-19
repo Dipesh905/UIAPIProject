2e8ab275f8992baf38500c6bd57d7c2f459a6b99
Dipesh Ghi <ghimiredipesh5@gmail.com>
2023-12-19 23:51:18 +0545

    Sucessfully Implemented popup menu with post detail and user detail

--- lib/view/screens/new_api_screen.dart
+++ lib/view/screens/new_api_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:uiapiapp/controller/provider/get_posts_provider.dart';
+import 'package:uiapiapp/controller/provider/get_single_post_provider.dart';
 import 'package:uiapiapp/controller/provider/get_users_provider.dart';
+import 'package:uiapiapp/controller/provider/userdetail_postdetails_provider.dart';
 import 'package:uiapiapp/controller/resources/colors.dart';
 import 'package:uiapiapp/controller/resources/images.dart';
 import 'package:uiapiapp/model/users/users_model.dart';
@@ -94,14 +96,192 @@ class NewApiScreen extends StatelessWidget {
                             padding: const EdgeInsets.all(8.0),
                             child: Text(posts[index].title),
                           )),
+                          Consumer(
+                            builder: (context, ref, child) {
+                              final userDetailPostDetailStatus =
+                                  ref.watch(userDetailPostDetailStatusProvider);
+                              return PopupMenuButton<
+                                  UserDetailPostDetailStatus>(
+                                icon: SizedBox(
+                                    height: 20,
+                                    width: 40,
+                                    child: Image.asset(
+                                      AppImage.threeDotImage,
+                                      fit: BoxFit.cover,
+                                    )),
+                                initialValue: userDetailPostDetailStatus,
+                                onSelected: (UserDetailPostDetailStatus item) {
+                                  print('===Item===$item===================');
+                                  print(
+                                      '===on popup menu selected id is ${posts[index].id}===================');
 
-                          /// pop up menu button implement garne
-                          InkWell(
-                            child: SizedBox(
-                              height: 30,
-                              width: 40,
-                              child: Image.asset(AppImage.threeDotImage),
-                            ),
+                                  ref
+                                      .read(userDetailPostDetailStatusProvider
+                                          .notifier)
+                                      .update((state) => item);
+
+                                  if (item ==
+                                      UserDetailPostDetailStatus.postDetails) {
+                                    print('==========post detail===========');
+                                    showBottomSheet(
+                                      context: context,
+                                      builder: (context) {
+                                        return Consumer(
+                                          builder: (context, ref, child) {
+                                            final response = ref.watch(
+                                                singlePostProvider(
+                                                    posts[index].id));
+
+                                            return response.when(
+                                              data: (post) {
+                                                return Padding(
+                                                  padding:
+                                                      const EdgeInsets.all(8.0),
+                                                  child: Column(
+                                                    crossAxisAlignment:
+                                                        CrossAxisAlignment
+                                                            .start,
+                                                    children: [
+                                                      const Text(
+                                                          'Currently Selected Post Detail'),
+                                                      Text('Id: ${post?.id}'),
+                                                      Text(
+                                                          'Title: ${post?.title}'),
+                                                      Text(
+                                                          'Body: ${post?.body}'),
+                                                    ],
+                                                  ),
+                                                );
+                                              },
+                                              error: (error, stackTrace) =>
+                                                  const Text(
+                                                      'Error Obtaining data'),
+                                              loading: () => const Center(
+                                                child:
+                                                    CircularProgressIndicator(),
+                                              ),
+                                            );
+                                          },
+                                        );
+                                      },
+                                    );
+                                  } else if (item ==
+                                      UserDetailPostDetailStatus.userDetails) {
+                                    print(
+                                        '============user detail================');
+                                    showBottomSheet(
+                                      context: context,
+                                      builder: (context) {
+                                        return Consumer(
+                                          builder: (context, ref, child) {
+                                            final asyncUsers =
+                                                ref.watch(asyncUsersProvider);
+
+                                            return asyncUsers.when(
+                                              data: (users) {
+                                                User userWithSameUserIdInPost =
+                                                    users
+                                                        .where(
+                                                          (element) =>
+                                                              element.id ==
+                                                              posts[index]
+                                                                  .userId,
+                                                        )
+                                                        .first;
+
+                                                return Padding(
+                                                  padding:
+                                                      const EdgeInsets.all(8.0),
+                                                  child: Column(
+                                                    crossAxisAlignment:
+                                                        CrossAxisAlignment
+                                                            .start,
+                                                    children: [
+                                                      const Text(
+                                                          'Personal Information'),
+                                                      Text(
+                                                          'Id: ${userWithSameUserIdInPost.id}'),
+                                                      Text(
+                                                          'Name: ${userWithSameUserIdInPost.name}'),
+                                                      Text(
+                                                          'UserName: ${userWithSameUserIdInPost.username}'),
+                                                      const Text(
+                                                          'Personal Information'),
+                                                      Text(
+                                                          'Email: ${userWithSameUserIdInPost.email}'),
+                                                      Text(
+                                                          'phone: ${userWithSameUserIdInPost.phone}'),
+                                                      Text(
+                                                          'website: ${userWithSameUserIdInPost.website}'),
+                                                      const Padding(
+                                                        padding:
+                                                            EdgeInsets.only(
+                                                                top: 8.0),
+                                                        child: Text('Address'),
+                                                      ),
+                                                      Text(
+                                                          'Street: ${userWithSameUserIdInPost.address.street}'),
+                                                      Text(
+                                                          'suite: ${userWithSameUserIdInPost.address.suite}'),
+                                                      Text(
+                                                          'city: ${userWithSameUserIdInPost.address.city}'),
+                                                      Text(
+                                                          'zipcode: ${userWithSameUserIdInPost.address.zipcode}'),
+                                                      Text(
+                                                          'lat: ${userWithSameUserIdInPost.address.geo.lat}'),
+                                                      Text(
+                                                          'lng: ${userWithSameUserIdInPost.address.geo.lng}'),
+                                                      const Padding(
+                                                        padding:
+                                                            EdgeInsets.only(
+                                                                top: 8.0),
+                                                        child: Text(
+                                                            'Company Details'),
+                                                      ),
+                                                      Text(
+                                                          'name: ${userWithSameUserIdInPost.company.name}'),
+                                                      Text(
+                                                          'catchPhrase: ${userWithSameUserIdInPost.company.catchPhrase}'),
+                                                      Text(
+                                                          'bs: ${userWithSameUserIdInPost.company.bs}'),
+                                                    ],
+                                                  ),
+                                                );
+                                              },
+                                              error: (error, stackTrace) {
+                                                return const Text(
+                                                    'Error Obtaining the data');
+                                              },
+                                              loading: () {
+                                                return const Center(
+                                                    child:
+                                                        CircularProgressIndicator());
+                                              },
+                                            );
+                                          },
+                                        );
+                                      },
+                                    );
+                                  }
+                                },
+                                itemBuilder: (BuildContext context) =>
+                                    <PopupMenuEntry<
+                                        UserDetailPostDetailStatus>>[
+                                  const PopupMenuItem<
+                                      UserDetailPostDetailStatus>(
+                                    value:
+                                        UserDetailPostDetailStatus.postDetails,
+                                    child: Text('Post Details'),
+                                  ),
+                                  const PopupMenuItem<
+                                      UserDetailPostDetailStatus>(
+                                    value:
+                                        UserDetailPostDetailStatus.userDetails,
+                                    child: Text('User Details'),
+                                  ),
+                                ],
+                              );
+                            },
                           )
                         ],
                       ),
